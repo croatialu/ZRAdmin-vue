280cbffcea0db9d6879947fe8372ad7a3065ab33
croatialu <wuli.croatia@foxmail.com>
2022-09-06 21:17:23 +0800

    chore: 增加eslint 配置

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
 import createVitePlugins from './vite/plugins'
 
 // https://vitejs.dev/config/
@@ -10,7 +10,7 @@ export default defineConfig(({ mode, command }) => {
     // 设置路径
     '~': path.resolve(__dirname, './'),
     // 设置别名
-    '@': path.resolve(__dirname, './src')
+    '@': path.resolve(__dirname, './src'),
   }
   if (command === 'serve') {
     // 解决警告You are running the esm-bundler build of vue-i18n.
@@ -20,28 +20,28 @@ export default defineConfig(({ mode, command }) => {
     plugins: createVitePlugins(env, command === 'build'),
     resolve: {
       // https://cn.vitejs.dev/config/#resolve-alias
-      alias: alias,
+      alias,
       // 导入时想要省略的扩展名列表
       // https://cn.vitejs.dev/config/#resolve-extensions
-      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     },
     css: {
-      devSourcemap: true //开发模式时启用
+      devSourcemap: true, // 开发模式时启用
     },
     base: env.VITE_APP_ROUTER_PREFIX,
     // 打包配置
     build: {
       sourcemap: command === 'build' ? false : 'inline',
-      outDir: 'dist', //指定输出目录
-      assetsDir: 'assets', //指定静态资源存储目录(相对于outDir)
+      outDir: 'dist', // 指定输出目录
+      assetsDir: 'assets', // 指定静态资源存储目录(相对于outDir)
       // 将js、css文件分离到单独文件夹
       rollupOptions: {
         output: {
           chunkFileNames: 'static/js/[name]-[hash].js',
           entryFileNames: 'static/js/[name]-[hash].js',
-          assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
-        }
-      }
+          assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        },
+      },
     },
     // vite 相关配置
     server: {
@@ -53,14 +53,14 @@ export default defineConfig(({ mode, command }) => {
         '/dev-api': {
           target: 'http://localhost:8888',
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/dev-api/, '')
+          rewrite: path => path.replace(/^\/dev-api/, ''),
         },
         '/msghub': {
           target: 'http://localhost:8888',
           ws: true,
-          rewrite: (path) => path.replace(/^\/msgHub/, '')
-        }
-      }
-    }
+          rewrite: path => path.replace(/^\/msgHub/, ''),
+        },
+      },
+    },
   }
 })
